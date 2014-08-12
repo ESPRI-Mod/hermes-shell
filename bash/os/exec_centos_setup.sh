bb2f5f103c8aa9e36ee78f82bd836a20358acb5e
Mark Morgan <momipsl@ipsl.jussieu.fr>
2014-08-12 13:44:20 +0200

    Improved server setup script

--- bash/os/exec_centos_setup.sh
+++ bash/os/exec_centos_setup.sh
@@ -1,79 +1,38 @@
 #!/bin/bash
 
-# ###############################################################
-# SECTION: MAIN
-# ###############################################################
-
+# Declare helper vars.
 declare SERVER_TYPE=$1
-echo $SERVER_TYPE
-log "Setting up "$SERVER_TYPE" server ..."
+declare REPO=https://github.com/Prodiguer/prodiguer-shell.git
 
-exit 0
-
-
-# Login as root.
-su
-
-# Goto working directory.
+# Set working directory.
 cd /opt
 
 # Ensure that git is available.
 yum install git
 
 # Clone prodiguer shell.
-git clone https://github.com/Prodiguer/prodiguer-shell.git prodiguer
-
-# Set alias.
-alias prodiguer=./prodiguer/exec.sh
+rm -rf prodiguer
+git clone $REPO prodiguer
 
 # Run server setup.
 if [ $SERVER_TYPE = "db" ]; then
-	prodiguer setup-centos-db-server
+	./prodiguer/exec.sh setup-centos-db-server
 elif [ $SERVER_TYPE = "mq" ]; then
-	prodiguer setup-centos-mq-server
+	./prodiguer/exec.sh setup-centos-mq-server
 elif [ $SERVER_TYPE = "web" ]; then
-	prodiguer setup-centos-web-server
+	./prodiguer/exec.sh setup-centos-web-server
 fi
 
 # Install stack.
-prodiguer stack-bootstrap
-prodiguer stack-install
+./prodiguer/exec.sh stack-bootstrap
+./prodiguer/exec.sh stack-install
 
 # Install db.
-prodiguer run-db-install
+./prodiguer/exec.sh run-db-install
 
 # Run post setup tasks.
 if [ $SERVER_TYPE = "db" ]; then
-	prodiguer run-tests db
+	./prodiguer/exec.sh run-tests db
 elif [ $SERVER_TYPE = "web" ]; then
 	nginx -s reload
 fi
-
-# ###############################################################
-# SECTION: HELPERS
-# ###############################################################
-
-# Wraps standard echo by adding PRODIGUER prefix.
-log()
-{
-	declare tabs=''
-	if [ "$1" ]; then
-		if [ "$2" ]; then
-			for ((i=0; i<$2; i++))
-			do
-				declare tabs+='\t'
-			done
-	    	echo -e 'IPSL PRODIGUER INFO SH > '$tabs$1
-	    else
-	    	echo -e "IPSL PRODIGUER INFO SH > "$1
-	    fi
-	else
-	    echo -e ""
-	fi
-}
-
-# Outputs a line to split up logging.
-log_banner()
-{
-	echo "-------------------------------------------------------------------------------"
-}
