8e43823d7831219ad9b2a25486d45a1eecfa1165
Mark Morgan <momipsl@ipsl.jussieu.fr>
2014-09-23 15:45:59 +0200

    added command to install mongo db server

--- bash/run_db_mongo.sh
+++ bash/run_db_mongo.sh
@@ -12,3 +12,12 @@ run_db_mongo_restart()
 {
 	sudo service mongod restart
 }
+
+run_db_mongo_install()
+{
+	# Configure the package management system (YUM).
+	cp $DIR_RESOURCES/other/yum-repo-mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+	# Install latest stable version of mongodb.
+	yum install -y mongodb-org
+}
