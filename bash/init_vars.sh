397d42fffa1c053d1a20d1687d6a5b06a199db4c
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-08-06 13:59:33 +0200

    Removed const from set of managed repos

--- bash/init_vars.sh
+++ bash/init_vars.sh
@@ -10,7 +10,6 @@ test -f $DIR_CONFIG/prodiguer.sh && source $DIR_CONFIG/prodiguer.sh
 # Set of git repos.
 declare -a REPOS=(
 	'prodiguer-client'
-	'conso'
 	'prodiguer-cv'
 	'prodiguer-docs'
 	'prodiguer-fe'
