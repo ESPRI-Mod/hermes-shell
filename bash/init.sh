b3efee5c39bc43a5c845dc923380a2a83099ca3a
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-08-18 13:59:10 +0200

    Removed obsolete files

--- bash/init.sh
+++ bash/init.sh
@@ -5,21 +5,6 @@ source $PRODIGUER_HOME/bash/init_helpers.sh
 source $PRODIGUER_HOME/bash/init_paths.sh
 source $PRODIGUER_HOME/bash/init_vars.sh
 
-source $PRODIGUER_HOME/bash/run_cv.sh
-source $PRODIGUER_HOME/bash/run_db_mongo.sh
-source $PRODIGUER_HOME/bash/run_db_pgres.sh
-source $PRODIGUER_HOME/bash/run_db_pgres_reset.sh
-source $PRODIGUER_HOME/bash/run_mq.sh
-source $PRODIGUER_HOME/bash/run_os.sh
 source $PRODIGUER_HOME/bash/run_stack.sh
 source $PRODIGUER_HOME/bash/run_utest.sh
-source $PRODIGUER_HOME/bash/run_web.sh
 
-source $PRODIGUER_HOME/bash/help.sh
-source $PRODIGUER_HOME/bash/help_cv.sh
-source $PRODIGUER_HOME/bash/help_db.sh
-source $PRODIGUER_HOME/bash/help_mq.sh
-source $PRODIGUER_HOME/bash/help_os.sh
-source $PRODIGUER_HOME/bash/help_stack.sh
-source $PRODIGUER_HOME/bash/help_utest.sh
-source $PRODIGUER_HOME/bash/help_web.sh
