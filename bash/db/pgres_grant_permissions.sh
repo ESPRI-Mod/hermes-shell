0552af53a141609fa6b5ca4d4a2d2663e61f8afc
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-08-21 16:21:55 +0200

    Updated script paths

--- bash/db/pgres_grant_permissions.sh
+++ bash/db/pgres_grant_permissions.sh
@@ -2,4 +2,4 @@ source $PRODIGUER_HOME/bash/init.sh
 
 log "DB : granting user permissions"
 
-psql -U prodiguer_db_admin -d prodiguer -q -f $PRODIGUER_HOME/db/pgres_grant_permissions.sql
+psql -U prodiguer_db_admin -d prodiguer -q -f $PRODIGUER_HOME/bash/db/pgres_grant_permissions.sql
