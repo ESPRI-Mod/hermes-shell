0aa269408c0216277299c12ae12a6e81fe6c52d6
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-08-24 10:55:51 +0200

    Fixed path to supervisord log file

--- bash/mq/daemons_reset_logs.sh
+++ bash/mq/daemons_reset_logs.sh
@@ -3,6 +3,6 @@ source $PRODIGUER_HOME/bash/init.sh
 
 # Delete log files.
 rm $PRODIGUER_DIR_LOGS/mq/*.log
-rm $PRODIGUER_DIR_DAEMONS/mq/supervisord.log
+rm $PRODIGUER_DIR_DAEMONS/mq/supervisor.log
 
 log "MQ : reset daemon logs"
--- bash/web/daemons_reset_logs.sh
+++ bash/web/daemons_reset_logs.sh
@@ -3,6 +3,6 @@ source $PRODIGUER_HOME/bash/init.sh
 
 # Delete log files.
 rm $PRODIGUER_DIR_LOGS/web/*.log
-rm $PRODIGUER_DIR_DAEMONS/web/supervisord.log
+rm $PRODIGUER_DIR_DAEMONS/web/supervisor.log
 
 log "WEB : reset daemon logs"
