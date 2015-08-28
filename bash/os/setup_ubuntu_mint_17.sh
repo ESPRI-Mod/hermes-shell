2155b0c36e2b881d4f8fb2d20a3cd253cb16d97c
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-08-28 13:43:25 +0200

    Added anent setup for common libs

--- bash/os/setup_ubuntu_mint_17.sh
+++ bash/os/setup_ubuntu_mint_17.sh
@@ -1,7 +1,35 @@
 # Installs common libraries.
 prodiguer_setup_common()
 {
-	echo "TODO: prodiguer_setup_common"
+	# Ensure machine is upto date.
+	apt-get -qq -y update
+	apt-get -qq -y upgrade
+
+	# Install various libs.
+	apt-get -qq -y install git
+	apt-get -qq -y install libz-dev
+	apt-get -qq -y install libffi-dev
+	apt-get -qq -y install zlib1g-dev
+	apt-get -qq -y install bzip2
+	apt-get -qq -y install openssl
+	apt-get -qq -y install libssl-dev
+	apt-get -qq -y install ncurses-dev
+	apt-get -qq -y install sqlite3
+	apt-get -qq -y install libsqlite-dev
+	apt-get -qq -y install libreadline-dev
+	apt-get -qq -y install tk
+	apt-get -qq -y install libgdbm-dev
+	apt-get -qq -y install libdb6.0-dev
+	apt-get -qq -y install libpcap-dev
+	apt-get -qq -y install postgresql-client
+	apt-get -qq -y install python-dev
+	apt-get -qq -y install python-pip
+	apt-get -qq -y install postgresql-plpython
+	apt-get -qq -y install python-psycopg2
+	apt-get -qq -y install g++
+	apt-get -qq -y install freetype*
+	apt-get -qq -y install libpng-dev
+	apt-get -qq -y install python-matplotlib
 }
 
 # Installs NGINX web server.
