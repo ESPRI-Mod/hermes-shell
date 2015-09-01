e38f8cf77a8926e761b0449881082a7c7f96f507
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-09-01 11:48:10 +0200

    Renamed delete mail table command.

--- bash/db/pgres_reset_message_email_table.sh
+++ bash/db/pgres_reset_message_email_table.sh
@@ -3,6 +3,6 @@ source $PRODIGUER_HOME/bash/init.sh
 log "DB : resetting postgres mq.tbl_message_email table ..."
 
 activate_venv server
-python $PRODIGUER_DIR_JOBS/db/run_pgres_reset_email_table.py
+python $PRODIGUER_DIR_JOBS/db/run_pgres_reset_message_email_table.py
 
 log "DB : reset postgres mq.tbl_message_email table"
