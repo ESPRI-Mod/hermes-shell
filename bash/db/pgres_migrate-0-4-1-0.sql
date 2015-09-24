f3f69b82cdf032f05fc4de7fb525b85976965733
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-09-24 16:39:00 +0200

    1.  New migrate script.  2.  Updated permission script to include supervisor schema.

---
+++ bash/db/pgres_migrate-0-4-1-0.sql
@@ -0,0 +1,27 @@
+-- Add column: monitoring.tbl_job.warning_delay
+ALTER TABLE monitoring.tbl_job ADD COLUMN warning_delay integer;
+
+-- Update column: monitoring.tbl_job.warning_delay
+-- Calculate based upon difference between expected and actual end dates
+UPDATE
+	monitoring.tbl_job
+SET
+	warning_delay = (SELECT EXTRACT(EPOCH FROM expected_execution_end_date)) - (SELECT EXTRACT(EPOCH FROM execution_start_date))
+WHERE
+	execution_start_date IS NOT NULL AND
+	expected_execution_end_date IS NOT NULL;
+
+-- Update column: monitoring.tbl_job.warning_delay
+-- Set to default value if undefined
+UPDATE
+	monitoring.tbl_job
+SET
+	warning_delay = 86400
+WHERE
+	warning_delay IS NULL;
+
+-- Drop column: monitoring.tbl_job.expected_execution_end_date
+ALTER TABLE monitoring.tbl_job DROP COLUMN expected_execution_end_date;
+
+-- Drop column: monitoring.tbl_job.was_late
+ALTER TABLE monitoring.tbl_job DROP COLUMN was_late;
--- bash/db/pgres_migrate.sql
+++
@@ -1,3 +0,0 @@
--- monitoring.tbl_job: add warning_delay column.
-ALTER TABLE monitoring.tbl_job ADD COLUMN warning_delay integer;
-
