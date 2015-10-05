8982492d44e91f4b3ba1892c0550425409fb3e16
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-10-05 14:38:48 +0200

    New command to update db permissions

--- bash/stack/update_config.sh
+++ bash/stack/update_config.sh
@@ -9,13 +9,12 @@ main()
 	log "UPDATING CONFIG"
 
 	cp $PRODIGUER_DIR_TEMPLATES/prodiguer.json $PRODIGUER_DIR_CONFIG/prodiguer.json
+
 	if [ $PRODIGUER_MACHINE_TYPE = "dev" ]; then
 		cp $PRODIGUER_DIR_TEMPLATES/mq-supervisord-dev.conf $PRODIGUER_DIR_DAEMONS/mq/supervisord.conf
+		cp $PRODIGUER_DIR_TEMPLATES/web-supervisord.conf $PRODIGUER_DIR_DAEMONS/web/supervisord.conf
 	elif [ $PRODIGUER_MACHINE_TYPE = "mq" ]; then
 		cp $PRODIGUER_DIR_TEMPLATES/mq-supervisord-mq.conf $PRODIGUER_DIR_DAEMONS/mq/supervisord.conf
-	fi
-	if [ $PRODIGUER_MACHINE_TYPE = "dev" ]; then
-		cp $PRODIGUER_DIR_TEMPLATES/web-supervisord.conf $PRODIGUER_DIR_DAEMONS/web/supervisord.conf
 	elif [ $PRODIGUER_MACHINE_TYPE = "web" ]; then
 		cp $PRODIGUER_DIR_TEMPLATES/web-supervisord.conf $PRODIGUER_DIR_DAEMONS/web/supervisord.conf
 	fi
