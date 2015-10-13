60d1ad3124b7b853a549f923faab55e4aca7c9e1
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-10-14 00:28:59 +0200

    Updated performance metrics test script

--- bash/db/pgres_performance_tests.sh
+++ bash/db/pgres_performance_tests.sh
@@ -1,8 +1,8 @@
 source $PRODIGUER_HOME/bash/init.sh
 
-log "DB : running postgres db performance tests ..."
+log "DB : executing postgres db performance tests ..."
 
 activate_venv server
 python $PRODIGUER_DIR_JOBS/db/run_pgres_performance_tests.py
 
-log "DB : migrated postgres db"
+log "DB : executed postgres db performance tests"
