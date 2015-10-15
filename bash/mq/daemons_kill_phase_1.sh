3829787a05e2add2b65e568766ca15154970ef47
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-10-16 00:46:23 +0200

    Added option of a 2 phase kill of MQ daemons

---
+++ bash/mq/daemons_kill_phase_1.sh
@@ -0,0 +1,9 @@
+# Import utils.
+source $PRODIGUER_HOME/bash/init.sh
+
+log "MQ : stopping live-smtp-realtime email listener ..."
+
+activate_venv server
+supervisorctl -c $PRODIGUER_DIR_DAEMONS/mq/supervisord.conf stop live-smtp-realtime:01
+
+log "MQ : stopped live-smtp-realtime email listener ..."
---
+++ bash/mq/daemons_kill_phase_2.sh
@@ -0,0 +1,6 @@
+# Import utils.
+source $PRODIGUER_HOME/bash/init.sh
+
+# Kill daemons.
+source $PRODIGUER_HOME/bash/mq/daemons_kill.sh
+
