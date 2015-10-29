5035d792950669efa6e43618cd025c1a238704f6
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-10-29 15:16:27 +0100

    Added new db schema const to list of db objects for which permissions are granted

--- bash/db/sql/pgres_grant_permissions.sql
+++ bash/db/sql/pgres_grant_permissions.sql
@@ -1,13 +1,15 @@
 /* Grant schema permissions */
-GRANT USAGE ON SCHEMA cv, monitoring, mq, superviseur TO prodiguer_db_user;
+GRANT USAGE ON SCHEMA conso, cv, monitoring, mq, superviseur TO prodiguer_db_user;
 
 /* Grant table permissions */
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA conso TO prodiguer_db_user;
 GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA cv TO prodiguer_db_user;
 GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA monitoring TO prodiguer_db_user;
 GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA mq TO prodiguer_db_user;
 GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA superviseur TO prodiguer_db_user;
 
 /* Grant sequence permissions */
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA conso TO prodiguer_db_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA cv TO prodiguer_db_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA monitoring TO prodiguer_db_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA mq TO prodiguer_db_user;
