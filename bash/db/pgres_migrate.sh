3613713cd260f7553eecf67b622a2fa894d809e4
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-11-03 15:27:26 +0100

    1.  Updated MQ broker definitions.  2.  Updated latest db-migrate script.

--- bash/db/pgres_migrate.sh
+++ bash/db/pgres_migrate.sh
@@ -2,7 +2,7 @@ source $PRODIGUER_HOME/bash/init.sh
 
 log "DB : migrating postgres db ..."
 
-psql -U prodiguer_db_admin -d prodiguer -q -f $PRODIGUER_HOME/bash/db/sql/pgres_migrate-0-4-3-2.sql
+psql -U prodiguer_db_admin -d prodiguer -q -f $PRODIGUER_HOME/bash/db/sql/pgres_migrate-0-4-3-3.sql
 source $PRODIGUER_HOME/bash/db/pgres_grant_permissions.sh
 
 log "DB : migrated postgres db"
---
+++ bash/db/sql/pgres_migrate-0-4-3-3.sql
@@ -0,0 +1,96 @@
+-- Create table: conso.tbl_allocation
+CREATE TABLE conso.tbl_allocation
+(
+  id serial NOT NULL,
+  centre character varying(127) NOT NULL,
+  end_date timestamp without time zone NOT NULL,
+  machine character varying(127) NOT NULL,
+  node_type character varying(127) NOT NULL,
+  project character varying(127) NOT NULL,
+  start_date timestamp without time zone NOT NULL,
+  total_hrs character varying(127) NOT NULL,
+  row_create_date timestamp without time zone NOT NULL,
+  row_update_date timestamp without time zone,
+  CONSTRAINT tbl_allocation_pkey PRIMARY KEY (id),
+  CONSTRAINT tbl_allocation_centre_machine_node_type_project_start_date_key UNIQUE (centre, machine, node_type, project, start_date)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE conso.tbl_allocation
+  OWNER TO prodiguer_db_admin;
+
+-- Create sequence: conso.tbl_allocation_id_seq
+CREATE SEQUENCE conso.tbl_allocation_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE conso.tbl_allocation_id_seq
+  OWNER TO prodiguer_db_admin;
+
+-- Create table: conso.tbl_consumption
+CREATE TABLE conso.tbl_consumption
+(
+  id serial NOT NULL,
+  allocation_id integer,
+  date timestamp without time zone NOT NULL,
+  total_hrs double precision NOT NULL,
+  login character varying(127),
+  row_create_date timestamp without time zone NOT NULL,
+  row_update_date timestamp without time zone,
+  CONSTRAINT tbl_consumption_pkey PRIMARY KEY (id),
+  CONSTRAINT tbl_consumption_allocation_id_fkey FOREIGN KEY (allocation_id)
+      REFERENCES conso.tbl_allocation (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE conso.tbl_consumption
+  OWNER TO prodiguer_db_admin;
+
+-- Create sequence: conso.tbl_consumption_id_seq
+CREATE SEQUENCE conso.tbl_consumption_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE conso.tbl_consumption_id_seq
+  OWNER TO prodiguer_db_admin;
+
+-- Drop table: conso.tbl_occupation_store
+DROP TABLE conso.tbl_occupation_store;
+
+-- Drop sequence: conso.tbl_occupation_store_id_seq
+DROP SEQUENCE conso.tbl_occupation_store_id_seq;
+
+-- Create table: conso.tbl_occupation_store
+CREATE TABLE conso.tbl_occupation_store
+(
+  id serial NOT NULL,
+  date timestamp without time zone NOT NULL,
+  login character varying(127) NOT NULL,
+  name character varying(127) NOT NULL,
+  size double precision NOT NULL,
+  row_create_date timestamp without time zone NOT NULL,
+  row_update_date timestamp without time zone,
+  CONSTRAINT tbl_occupation_store_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE conso.tbl_occupation_store
+  OWNER TO prodiguer_db_admin;
+
+-- Create sequence: conso.tbl_occupation_store_id_seq
+CREATE SEQUENCE conso.tbl_occupation_store_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE conso.tbl_occupation_store_id_seq
+  OWNER TO prodiguer_db_admin;
