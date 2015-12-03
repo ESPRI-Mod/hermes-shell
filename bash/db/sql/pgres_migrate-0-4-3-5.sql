d80e8ff99230ad547113f22f2d13c7681ae4d9b8
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-12-03 11:49:40 +0100

    updated conso allocation column table

--- bash/db/sql/pgres_migrate-0-4-3-5.sql
+++ bash/db/sql/pgres_migrate-0-4-3-5.sql
@@ -3,4 +3,3 @@ ALTER TABLE conso.tbl_allocation DROP COLUMN total_hrs;
 
 -- Create column: conso.tbl_allocation.total_hrs
 ALTER TABLE conso.tbl_allocation ADD COLUMN total_hrs double precision;
-ALTER TABLE conso.tbl_allocation ALTER COLUMN total_hrs SET NOT NULL;
