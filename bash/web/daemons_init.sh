6711ca86b4be324cb9f88342e4343851794d6fe0
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-01-27 22:45:17 +0100

    Removed implicit reset of web service logs when daemon is laughed

--- bash/web/daemons_init.sh
+++ bash/web/daemons_init.sh
@@ -1,9 +1,6 @@
 # Import utils.
 source $PRODIGUER_HOME/bash/init.sh
 
-# Reset logs.
-source $PRODIGUER_HOME/bash/web/daemons_reset_logs.sh
-
 # Launch daemons.
 activate_venv server
 supervisord -c $PRODIGUER_DIR_DAEMONS/web/supervisord.conf
