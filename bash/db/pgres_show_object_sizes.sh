b909b87257f5dce49df21767800e91086dd21043
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-02-10 15:28:25 +0100

    new command to display db object sizes

---
+++ bash/db/pgres_show_object_sizes.sh
@@ -0,0 +1,7 @@
+source $PRODIGUER_HOME/bash/init.sh
+
+log "DB : displaying postgres db object sizes ..."
+
+psql -U prodiguer_db_admin -d prodiguer -q -f $PRODIGUER_HOME/bash/db/sql/pgres_show_object_sizes.sql
+
+log "DB : displayed postgres db object sizes ..."
---
+++ bash/db/sql/pgres_show_object_sizes.sql
@@ -0,0 +1,4 @@
+SELECT relname AS "relation", pg_size_pretty(pg_relation_size(C.oid)) AS "size"
+  FROM pg_class C LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+  WHERE nspname NOT IN ('pg_catalog', 'information_schema') AND relname NOT LIKE 'pg_%'
+  ORDER BY pg_relation_size(C.oid) DESC;
