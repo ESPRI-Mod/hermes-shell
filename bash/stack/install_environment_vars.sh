d5296d723e75fd10b4bc3df9ff9d2600fc9b7403
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-05-25 14:35:08 +0200

    new command to install env vars

---
+++ bash/stack/install_environment_vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Import utils.
+source $HERMES_HOME/bash/init.sh
+
+# Main entry point.
+main()
+{
+	log "INSTALLING ENVIRONMENT VARS ..."
+
+	cp $HERMES_DIR_TEMPLATES/hermes_env.sh $HOME/.hermes
+
+	log "INSTALLED ENVIRONMENT VARS"
+}
+
+# Invoke entry point.
+main
