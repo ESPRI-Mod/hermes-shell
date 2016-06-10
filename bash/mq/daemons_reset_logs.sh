67b889b9d9aed040def147234a5df5f97b54f567
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-06-10 14:48:25 +0200

    Refined daemon log deletion

--- bash/mq/daemons_reset_logs.sh
+++ bash/mq/daemons_reset_logs.sh
@@ -2,7 +2,7 @@
 source $HERMES_HOME/bash/init.sh
 
 # Delete log files.
-rm $HERMES_DIR_LOGS/mq/*.log
+rm $HERMES_DIR_LOGS/mq/std*.log
 rm $HERMES_DIR_DAEMONS/mq/supervisor.log
 
 log "MQ : reset daemon logs"
--- bash/web/daemons_reset_logs.sh
+++ bash/web/daemons_reset_logs.sh
@@ -2,7 +2,7 @@
 source $HERMES_HOME/bash/init.sh
 
 # Delete log files.
-rm $HERMES_DIR_LOGS/web/*.log
+rm $HERMES_DIR_LOGS/web/std*.log
 rm $HERMES_DIR_DAEMONS/web/supervisor.log
 
 log "WEB : reset daemon logs"
