18dbb62c0b3719c1a4c8097633b84658f0d8b246
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-06-13 11:00:30 +0200

    Added new dedicated db users to restrict access according to role

--- bash/db/pgres_create_db_users.sh
+++ bash/db/pgres_create_db_users.sh
@@ -4,3 +4,5 @@ log "DB : creating database users"
 
 createuser -U postgres -d -s hermes_db_admin
 createuser -U hermes_db_admin -D -S -R hermes_db_user
+createuser -U hermes_db_admin -D -S -R hermes_db_web_user
+createuser -U hermes_db_admin -D -S -R hermes_db_mq_user
--- bash/db/sql/pgres_grant_permissions.sql
+++ bash/db/sql/pgres_grant_permissions.sql
@@ -1,3 +1,6 @@
+/* ------------------------------------ */
+/* hermes_db_user permissions		    */
+/* ------------------------------------ */
 /* Grant schema permissions */
 GRANT USAGE ON SCHEMA conso, cv, monitoring, mq, superviseur TO hermes_db_user;
 
@@ -14,3 +17,44 @@ GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA cv TO hermes_db_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA monitoring TO hermes_db_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA mq TO hermes_db_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA superviseur TO hermes_db_user;
+
+/* ------------------------------------ */
+/* hermes_db_mq_user permissions		*/
+/* ------------------------------------ */
+/* Grant schema permissions */
+GRANT USAGE ON SCHEMA conso, cv, monitoring, mq, superviseur TO hermes_db_mq_user;
+
+/* Grant table permissions */
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA conso TO hermes_db_mq_user;
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA cv TO hermes_db_mq_user;
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA monitoring TO hermes_db_mq_user;
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA mq TO hermes_db_mq_user;
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA superviseur TO hermes_db_mq_user;
+
+/* Grant sequence permissions */
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA conso TO hermes_db_mq_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA cv TO hermes_db_mq_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA monitoring TO hermes_db_mq_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA mq TO hermes_db_mq_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA superviseur TO hermes_db_mq_user;
+
+/* ------------------------------------ */
+/* hermes_db_web_user permissions		*/
+/* ------------------------------------ */
+/* Grant schema permissions */
+GRANT USAGE ON SCHEMA conso, cv, monitoring, mq, superviseur TO hermes_db_web_user;
+
+/* Grant table permissions */
+GRANT SELECT ON ALL TABLES IN SCHEMA conso TO hermes_db_web_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA cv TO hermes_db_web_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA monitoring TO hermes_db_web_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA mq TO hermes_db_web_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA superviseur TO hermes_db_web_user;
+
+/* Grant sequence permissions */
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA conso TO hermes_db_web_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA cv TO hermes_db_web_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA monitoring TO hermes_db_web_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA mq TO hermes_db_web_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA superviseur TO hermes_db_web_user;
+
