533f5299dd89127b53057993e1552fe074d9a764
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-06-14 00:00:13 +0200

    added new db users to set of db users to drop

--- bash/db/pgres_drop_db_users.sh
+++ bash/db/pgres_drop_db_users.sh
@@ -2,5 +2,7 @@ source $HERMES_HOME/bash/init.sh
 
 log "DB : dropping database users"
 
+dropuser -U hermes_db_admin hermes_db_mq_user
+dropuser -U hermes_db_admin hermes_db_web_user
 dropuser -U hermes_db_admin hermes_db_user
 dropuser -U postgres hermes_db_admin
