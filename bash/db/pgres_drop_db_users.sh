7e7bb39c22ef43e026914b235e8356a40754e2c6
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-07-06 17:24:04 +0200

    updated drop users command

--- bash/db/pgres_drop_db_users.sh
+++ bash/db/pgres_drop_db_users.sh
@@ -2,6 +2,7 @@ source $HERMES_HOME/bash/init.sh
 
 log "DB : dropping database users"
 
+dropuser -U hermes_db_admin hermes_db_conso_user
 dropuser -U hermes_db_admin hermes_db_mq_user
 dropuser -U hermes_db_admin hermes_db_web_user
 dropuser -U hermes_db_admin hermes_db_user
