8db467551adb4326972c124064a2fe87713d0d91
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-07-06 17:23:20 +0200

    added hermes_db_conso user account + permissions

--- bash/db/pgres_create_db_users.sh
+++ bash/db/pgres_create_db_users.sh
@@ -6,3 +6,4 @@ createuser -U postgres -d -s hermes_db_admin
 createuser -U hermes_db_admin -D -S -R hermes_db_user
 createuser -U hermes_db_admin -D -S -R hermes_db_web_user
 createuser -U hermes_db_admin -D -S -R hermes_db_mq_user
+createuser -U hermes_db_admin -D -S -R hermes_db_conso_user
--- bash/db/sql/pgres_grant_permissions.sql
+++ bash/db/sql/pgres_grant_permissions.sql
@@ -58,3 +58,23 @@ GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA monitoring TO hermes_db_web_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA mq TO hermes_db_web_user;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA superviseur TO hermes_db_web_user;
 
+
+/* ------------------------------------ */
+/* hermes_db_conso_user permissions		*/
+/* ------------------------------------ */
+/* Grant schema permissions */
+GRANT USAGE ON SCHEMA conso, cv, monitoring, mq, superviseur TO hermes_db_conso_user;
+
+/* Grant table permissions */
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA conso TO hermes_db_conso_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA cv TO hermes_db_conso_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA monitoring TO hermes_db_conso_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA mq TO hermes_db_conso_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA superviseur TO hermes_db_conso_user;
+
+/* Grant sequence permissions */
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA conso TO hermes_db_conso_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA cv TO hermes_db_conso_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA monitoring TO hermes_db_conso_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA mq TO hermes_db_conso_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA superviseur TO hermes_db_conso_user;
