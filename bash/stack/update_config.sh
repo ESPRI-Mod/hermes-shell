4367eb78c8aa31a5cf2728dac01eae8470c873bb
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-09-07 17:41:48 +0200

    MQ supervisord is now deployment agnostic when being updated

--- bash/stack/update_config.sh
+++ bash/stack/update_config.sh
@@ -11,10 +11,10 @@ main()
 	cp $HERMES_DIR_TEMPLATES/hermes.json $HERMES_DIR_CONFIG/hermes.json
 
 	if [ $HERMES_MACHINE_TYPE = "dev" ]; then
-		cp $HERMES_DIR_TEMPLATES/mq-supervisord.conf $HERMES_DIR_DAEMONS/mq/supervisord.conf
+		cp $HERMES_DIR_TEMPLATES/mq-supervisord-dev.conf $HERMES_DIR_DAEMONS/mq/supervisord.conf
 		cp $HERMES_DIR_TEMPLATES/web-supervisord.conf $HERMES_DIR_DAEMONS/web/supervisord.conf
 	elif [ $HERMES_MACHINE_TYPE = "mq" ]; then
-		cp $HERMES_DIR_TEMPLATES/mq-supervisord.conf $HERMES_DIR_DAEMONS/mq/supervisord.conf
+		cp $HERMES_DIR_TEMPLATES/mq-supervisord-$HERMES_DEPLOYMENT_MODE.conf $HERMES_DIR_DAEMONS/mq/supervisord.conf
 	elif [ $HERMES_MACHINE_TYPE = "web" ]; then
 		cp $HERMES_DIR_TEMPLATES/web-supervisord.conf $HERMES_DIR_DAEMONS/web/supervisord.conf
 	fi
