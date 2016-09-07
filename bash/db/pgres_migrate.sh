15b4d73ba45e54b3d063a1d7e3975c01b3e63129
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-09-07 16:46:19 +0200

    updated db migrate script

--- bash/db/pgres_migrate.sh
+++ bash/db/pgres_migrate.sh
@@ -2,11 +2,6 @@ source $HERMES_HOME/bash/init.sh
 
 log "DB : migrating postgres db ..."
 
-log "DB : creating new users"
-source $HERMES_HOME/bash/db/pgres_create_db_users.sh
-source $HERMES_HOME/bash/db/pgres_grant_permissions.sh
-
-log "DB : renaming database"
 psql -U hermes_db_admin -d postgres -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-1-0.sql
 
 log "DB : migrated postgres db"
