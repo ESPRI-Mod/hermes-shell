61fb0471287becaaa14d20b84aed59d2ecd5bf87
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-09-19 10:54:54 +0200

    updated ogres migrate script

--- bash/db/pgres_migrate.sh
+++ bash/db/pgres_migrate.sh
@@ -2,7 +2,7 @@ source $HERMES_HOME/bash/init.sh
 
 log "DB : migrating postgres db ..."
 
-psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-2-3-0.sql
-psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-2-3-1.sql
+psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-3-0-1.sql
+psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-3-0-2.sql
 
 log "DB : migrated postgres db"
