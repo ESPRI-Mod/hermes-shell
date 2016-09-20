c49b0d06e1792b096838c026d3ef4aae0ab4e8ea
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-09-20 21:28:53 +0200

    New db columns to store simulation paths

--- bash/db/pgres_migrate.sh
+++ bash/db/pgres_migrate.sh
@@ -2,10 +2,6 @@ source $HERMES_HOME/bash/init.sh
 
 log "DB : migrating postgres db ..."
 
-psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-3-0-1.sql
-psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-3-0-2.sql
-psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-3-0-3.sql
-psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-3-0-4.sql
-source $HERMES_HOME/bash/db/pgres_grant_permissions.sh
+psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_migrate-1-0-3-2.sql
 
 log "DB : migrated postgres db"
---
+++ bash/db/sql/pgres_migrate-1-0-3-2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE monitoring.tbl_simulation ADD COLUMN submission_path character varying(2047);
+ALTER TABLE monitoring.tbl_simulation ADD COLUMN archive_path character varying(2047);
+ALTER TABLE monitoring.tbl_simulation ADD COLUMN storage_path character varying(2047);
+ALTER TABLE monitoring.tbl_simulation ADD COLUMN storage_small_path character varying(2047);
