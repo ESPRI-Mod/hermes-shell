ef4c75963dee5d8737c433f0685081aecf1c45c5
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-10-19 23:08:46 +0200

    improved logging

--- bash/db/pgres_rectify_phase_1.sh
+++ bash/db/pgres_rectify_phase_1.sh
@@ -4,4 +4,4 @@ log "DB : rectifying postgres db - phase 1 ..."
 
 psql -U hermes_db_admin -d $HERMES_DB_PGRES_NAME -q -f $HERMES_HOME/bash/db/sql/pgres_rectify.sql
 
-log "DB : rectifying postgres db - phase 1"
+log "DB : rectified postgres db - phase 1"
--- bash/db/pgres_rectify_phase_2.sh
+++ bash/db/pgres_rectify_phase_2.sh
@@ -5,4 +5,4 @@ log "DB : rectifying postgres db - phase 2 ..."
 activate_venv server
 python $HERMES_HOME/bash/db/pgres_rectify_phase_2.py --throttle=$1
 
-log "DB : rectifying postgres db - phase 2"
+log "DB : rectified postgres db - phase 2"
