3387e7266c093ce603fbe5977e6abb229100509f
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-11-20 23:08:58 +0100

    New command to delete a simulation from db

---
+++ bash/db/pgres_delete_simulation.sh
@@ -0,0 +1,8 @@
+source $HERMES_HOME/bash/init.sh
+
+log "DB : deleting simulation from db ..."
+
+activate_venv server
+python $HERMES_DIR_JOBS/db/run_pgres_delete_simulation.py --uid=$1
+
+log "DB : simulation deleted from db ..."
