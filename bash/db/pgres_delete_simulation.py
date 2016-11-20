dfc0e103fd94f9c816688d8cd23fb5d4855e50dd
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-11-20 23:11:46 +0100

    New command to delete a simulation from db

---
+++ bash/db/pgres_delete_simulation.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: pgres_delete_simulation.py
+   :copyright: Copyright "Mar 21, 2015", Institute Pierre Simon Laplace
+   :license: GPL/CeCIL
+   :platform: Unix
+   :synopsis: Deletes simulation from HERMES postgres database tables.
+
+.. moduleauthor:: Mark Conway-Greenslade <momipsl@ipsl.jussieu.fr>
+
+
+"""
+import argparse
+
+from prodiguer.db import pgres as db
+from prodiguer.db.pgres import dao_monitoring as dao
+from prodiguer.utils import logger
+
+
+
+# Define command line arguments.
+_ARGS = argparse.ArgumentParser("Deletes a simulation from database.")
+_ARGS.add_argument(
+    "--uid",
+    help="UID of simulation to be deleted.",
+    dest="uid",
+    type=str
+    )
+
+
+def _main(args):
+    """Main entry point.
+
+    """
+    logger.log_db("Deleting simulation: begins")
+
+    with db.session.create(commitable=True):
+        dao.delete_simulation(args.uid)
+
+    logger.log_db("Deleting simulation: ends")
+
+
+if __name__ == '__main__':
+    _main(_ARGS.parse_args())
--- bash/db/pgres_delete_simulation.sh
+++ bash/db/pgres_delete_simulation.sh
@@ -3,6 +3,6 @@ source $HERMES_HOME/bash/init.sh
 log "DB : deleting simulation from db ..."
 
 activate_venv server
-python $HERMES_DIR_JOBS/db/run_pgres_delete_simulation.py --uid=$1
+python $HERMES_HOME/bash/db/pgres_delete_simulation.py --uid=$1
 
 log "DB : simulation deleted from db ..."
