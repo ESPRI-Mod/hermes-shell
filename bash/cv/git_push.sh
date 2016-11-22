6c09d3c082fa6556d9ec63c3c56cf94f238a921e
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-11-22 15:15:49 +0100

    Ensured that local prey is activated before pushing

--- bash/cv/git_push.sh
+++ bash/cv/git_push.sh
@@ -4,7 +4,7 @@ log "CV : pushing new terms to hermes-cv repo: "
 
 set_working_dir $HERMES_DIR_REPOS/hermes-cv
 
-hermes-local-proxify
+export http_proxy=http://proxy.ipsl.jussieu.fr:3128/ && export https_proxy=https://proxy.ipsl.jussieu.fr:3128/
 git add . --force
 git commit -m "hermes-cv-push--->`date +\%Y-\%m-\%dT\%H:\%M:\%S`"
 git push -v origin master:master
