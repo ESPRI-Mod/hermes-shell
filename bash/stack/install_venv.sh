733a93c69330e0566fe84e0c6df46bc495502613
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-01-19 14:12:41 +0100

    virtual env upgrade

--- bash/stack/install_venv.sh
+++ bash/stack/install_venv.sh
@@ -6,24 +6,24 @@ source $HERMES_HOME/bash/init.sh
 # Main entry point.
 main()
 {
-	log "Installing virtual environment"
+    log "Installing virtual environment"
 
-	# Set variables.
-	declare HERMES_DIR_VENV=$HERMES_HOME/ops/python/venv
-	declare HERMES_DIR_PYTHON=$HERMES_HOME/ops/python
-	declare HERMES_REQUIREMENTS=$HERMES_HOME/templates/venv-requirements-server.txt
+    # Set variables.
+    declare HERMES_DIR_VENV=$HERMES_HOME/ops/python/venv
+    declare HERMES_DIR_PYTHON=$HERMES_HOME/ops/python
+    declare HERMES_REQUIREMENTS=$HERMES_HOME/templates/venv-requirements-server.txt
 
-	# Reset.
-	rm -rf $HERMES_DIR_VENV
-	mkdir -p $HERMES_DIR_VENV
+    # Reset.
+    rm -rf $HERMES_DIR_VENV
+    mkdir -p $HERMES_DIR_VENV
 
-	# Set paths.
+    # Set paths.
     export PATH=$HERMES_DIR_PYTHON/bin:$PATH
-	export PYTHONPATH=$HERMES_DIR_PYTHON:$PYTHONPATH
+    export PYTHONPATH=$HERMES_DIR_PYTHON:$PYTHONPATH
 
-	# Upgrade pip / virtual env.
-	pip install --upgrade pip
-	pip install --upgrade virtualenv
+    # Upgrade pip / virtual env.
+    pip install --upgrade pip
+    pip install --upgrade virtualenv
 
     # Initialize venv.
     virtualenv -q $HERMES_DIR_VENV
@@ -32,8 +32,8 @@ main()
     # Install dependencies.
     pip install --allow-all-external -r $HERMES_REQUIREMENTS
 
-	# Clean up.
-	deactivate
+    # Clean up.
+    deactivate
 }
 
 # Invoke entry point.
--- bash/stack/update_venv.sh
+++ bash/stack/update_venv.sh
@@ -6,30 +6,30 @@ source $HERMES_HOME/bash/init.sh
 # Main entry point.
 main()
 {
-	log "Updating virtual environment"
+    log "Updating virtual environment"
 
-	# Set variables.
-	declare HERMES_DIR_VENV=$HERMES_HOME/ops/python/venv
-	declare HERMES_DIR_PYTHON=$HERMES_HOME/ops/python
-	declare HERMES_PYTHON=$HERMES_HOME/ops/python/bin/python
-	declare HERMES_REQUIREMENTS=$HERMES_HOME/templates/venv-requirements-server.txt
+    # Set variables.
+    declare HERMES_DIR_VENV=$HERMES_HOME/ops/python/venv
+    declare HERMES_DIR_PYTHON=$HERMES_HOME/ops/python
+    declare HERMES_PYTHON=$HERMES_HOME/ops/python/bin/python
+    declare HERMES_REQUIREMENTS=$HERMES_HOME/templates/venv-requirements-server.txt
 
-	# Set paths.
+    # Set paths.
     export PATH=$HERMES_DIR_PYTHON/bin:$PATH
-	export PYTHONPATH=$HERMES_DIR_PYTHON:$PYTHONPATH
+    export PYTHONPATH=$HERMES_DIR_PYTHON:$PYTHONPATH
 
     # Activate venv.
     source $HERMES_DIR_VENV/bin/activate
 
-	# Upgrade pip / virtual env.
-	pip install --upgrade pip
-	pip install --upgrade virtualenv
+    # Upgrade pip / virtual env.
+    pip install --upgrade pip
+    pip install --upgrade virtualenv
 
     # Upgrade dependencies.
     pip install --allow-all-external --upgrade -r $HERMES_REQUIREMENTS
 
-	# Clean up.
-	deactivate
+    # Clean up.
+    deactivate
 }
 
 # Invoke entry point.
