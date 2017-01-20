20074024abdf75219f3299af811019da57ce1cea
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-01-20 11:38:44 +0100

    Refined install python script

--- bash/stack/install_python.sh
+++ bash/stack/install_python.sh
@@ -9,18 +9,16 @@ main()
     log "Installing python ..."
 
     # Set variables.
-    declare PYTHON_VERSION=2.7.12
+    declare PYTHON_VERSION=2.7.13
 
     log "Installing python "$PYTHON_VERSION" (takes approx 2 minutes)"
 
     # Reset.
     rm -rf $HERMES_DIR_PYTHON
-    mkdir -p $HERMES_DIR_PYTHON
+    mkdir -p $HERMES_DIR_PYTHON/src
 
     # Download source.
-    cd $HERMES_DIR_PYTHON
-    mkdir src
-    cd src
+    cd $HERMES_DIR_PYTHON/src
     wget http://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz --no-check-certificate
     tar -xvf Python-$PYTHON_VERSION.tgz
 
@@ -29,23 +27,21 @@ main()
     ./configure --prefix=$HERMES_DIR_PYTHON
     make
     make install
+
+    # Update paths.
     export PATH=$HERMES_DIR_PYTHON/bin:$PATH
-    export PYTHONPATH=$PYTHONPATH:$HERMES_DIR_PYTHON
-    export PYTHONPATH=$PYTHONPATH:$HERMES_DIR_PYTHON/lib/python2.7/site-packages
+    export PYTHONPATH=$HERMES_DIR_PYTHON/bin:$PYTHONPATH
+    export PYTHONPATH=$HERMES_DIR_PYTHON/lib/python2.7/site-packages:$PYTHONPATH
 
     # Install setuptools.
     cd $HERMES_DIR_PYTHON/src
-    wget https://bootstrap.pypa.io/ez_setup.py
+    wget http://bootstrap.pypa.io/ez_setup.py
     python ez_setup.py
 
     # Install & upgrade pip / virtual env.
     easy_install --prefix $HERMES_DIR_PYTHON pip
     pip install --upgrade pip
     pip install --upgrade virtualenv
-
-    # Clean up.
-    cd $HERMES_DIR_PYTHON
-    rm -rf ./src
 }
 
 # Invoke entry point.
