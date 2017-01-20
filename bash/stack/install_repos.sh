c3aa06ad9602d65cad8fe58af80510d663585f84
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-01-20 11:42:09 +0100

    Simplified install repos script

--- bash/stack/install_repos.sh
+++ bash/stack/install_repos.sh
@@ -10,17 +10,9 @@ main()
 
     for repo in "${HERMES_REPOS[@]}"
     do
-        _install_repo $repo
+	    rm -rf $HERMES_DIR_REPOS/$repo
+	    git clone -q $HERMES_GITHUB/$repo.git $HERMES_DIR_REPOS/$repo
     done
-}
-
-# Installs a git repo.
-_install_repo()
-{
-    log "Installing repo: $1"
-
-    rm -rf $HERMES_DIR_REPOS/$1
-    git clone -q $HERMES_GITHUB/$1.git $HERMES_DIR_REPOS/$1
 }
 
 # Invoke entry point.
