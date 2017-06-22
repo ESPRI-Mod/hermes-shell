5e802615c71a6e6c498725a52e1fbf1bd15d1722
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2017-06-22 15:34:29 +0200

    Updated delete simulation to include previous tries

--- bash/db/pgres_delete_simulation.py
+++ bash/db/pgres_delete_simulation.py
@@ -38,7 +38,7 @@ def _main(args):
     with db.session.create(commitable=True):
         simulation = retrieve_simulation(args.uid)
         if simulation is not None:
-            for simulation in retrieve_simulations_by_hashid(simulation.hashid)
+            for simulation in retrieve_simulations_by_hashid(simulation.hashid):
                 dao.delete_simulation(simulation.uid)
 
     logger.log_db("Deleting simulation: ends")
