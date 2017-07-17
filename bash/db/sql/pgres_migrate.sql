a71767ea0807ef42c129934ae8249ffd96706ded
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2017-07-17 12:49:51 +0200

    Updated migrate script so that is_error flag is not set for late scenarios

--- bash/db/sql/pgres_migrate.sql
+++ bash/db/sql/pgres_migrate.sql
@@ -1,2 +1,28 @@
 ALTER TABLE monitoring.tbl_job ADD COLUMN is_late boolean;
 ALTER TABLE monitoring.tbl_job ALTER COLUMN is_late SET DEFAULT false;
+ALTER TABLE monitoring.tbl_job ADD COLUMN execution_end_date_limit timestamp without time zone;
+
+UPDATE
+	monitoring.tbl_job
+SET
+	execution_end_date_limit = execution_start_date + warning_delay * interval '1 second'
+WHERE
+	execution_start_date IS NOT NULL;
+
+UPDATE
+	monitoring.tbl_job
+SET
+	is_late = TRUE
+WHERE
+	execution_start_date IS NOT NULL AND
+	execution_end_date IS NOT NULL AND
+	execution_end_date > execution_end_date_limit;
+
+UPDATE
+	monitoring.tbl_job
+SET
+	is_late = TRUE
+WHERE
+	execution_start_date IS NOT NULL AND
+	execution_end_date IS NULL AND
+	now() > execution_end_date_limit;
