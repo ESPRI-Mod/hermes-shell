14114d9c1cf4f84948e29124e94d50a98097a0d5
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2017-07-19 15:25:20 +0200

    New simulation state for lateness

--- bash/db/sql/pgres_migrate.sql
+++ bash/db/sql/pgres_migrate.sql
@@ -1,28 +1,40 @@
-ALTER TABLE monitoring.tbl_job ADD COLUMN is_late boolean;
-ALTER TABLE monitoring.tbl_job ALTER COLUMN is_late SET DEFAULT false;
-ALTER TABLE monitoring.tbl_job ADD COLUMN execution_end_date_limit timestamp without time zone;
+-- Add new columns related to job executionlate issues.
+ALTER TABLE monitoring.tbl_job ADD COLUMN warning_limit timestamp without time zone;
+ALTER TABLE monitoring.tbl_job ADD COLUMN warning_state integer;
+ALTER TABLE monitoring.tbl_job ALTER COLUMN warning_state SET DEFAULT 0;
 
+-- Calculate job execution end date limits.
 UPDATE
 	monitoring.tbl_job
 SET
-	execution_end_date_limit = execution_start_date + warning_delay * interval '1 second'
+	warning_limit = execution_start_date + warning_delay * interval '1 second'
 WHERE
+	warning_limit IS NULL AND
 	execution_start_date IS NOT NULL;
 
+-- Set warning_state indicator where jobs are under supervision.
 UPDATE
-	monitoring.tbl_job
+	monitoring.tbl_job as j
 SET
-	is_late = TRUE
+	warning_state = 1
 WHERE
-	execution_start_date IS NOT NULL AND
-	execution_end_date IS NOT NULL AND
-	execution_end_date > execution_end_date_limit;
+    j.job_uid IN (SELECT job_uid FROM superviseur.tbl_supervision);
 
+-- Set warning_state indicator where jobs are late.
 UPDATE
 	monitoring.tbl_job
 SET
-	is_late = TRUE
+	warning_state = 2
 WHERE
+	warning_state = 0 AND
 	execution_start_date IS NOT NULL AND
 	execution_end_date IS NULL AND
-	now() > execution_end_date_limit;
+	now() > warning_limit;
+
+-- Set execution state of late jobs.
+UPDATE
+	monitoring.tbl_job
+SET
+	execution_state = 'l'
+WHERE
+	execution_end_date IS NULL AND warning_state > 0;
