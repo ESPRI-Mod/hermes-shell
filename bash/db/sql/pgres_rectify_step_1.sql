84b1669ebd06ec634fa3193b4fccd3e7e9ca338a
Mark A. Greenslade <momipsl@ipsl.jussieu.fr>
2017-12-21 10:14:40 +0100

    Corrected issue with phase 1 mq daemon stop

--- bash/db/sql/pgres_rectify_step_1.sql
+++ bash/db/sql/pgres_rectify_step_1.sql
@@ -1,9 +1,34 @@
+-- Check messages in error.
+SELECT DISTINCT processing_error FROM mq.tbl_message WHERE processing_error IS NOT NULL;
+
 -- Reset is queued for processing flag.
 UPDATE mq.tbl_message SET is_queued_for_reprocessing = FALSE WHERE is_queued_for_reprocessing = TRUE;
 
 -- 0000: failed due to vocab issue.
-UPDATE mq.tbl_message AS m SET is_queued_for_reprocessing = true WHERE m.type_id = '0000' AND m.processing_error LIKE '%nmanur%';
+UPDATE mq.tbl_message AS m SET is_queued_for_reprocessing = TRUE WHERE m.type_id = '0000' AND m.processing_error LIKE '%nmanur%';
 
--- 8000: falied due to incorrect python import.
-UPDATE mq.tbl_message AS m SET is_queued_for_reprocessing = true WHERE processing_error LIKE 'global name%';
+-- 8000: failed due to incorrect python import.
+UPDATE mq.tbl_message AS m SET is_queued_for_reprocessing = TRUE WHERE processing_error LIKE 'global name%';
+
+-- 0000: failed due to incorrect output dates.
+UPDATE mq.tbl_message AS m SET is_queued_for_reprocessing = TRUE WHERE processing_error LIKE '%Could not match input to any of%';
+
+-- 0000: failed due to february date.
+update mq.tbl_message set is_queued_for_reprocessing = TRUE WHERE processing_error LIKE '%is out of range%' and type_id = '0000';
+
+-- 2000: failed due to february date.
+update mq.tbl_message set is_queued_for_reprocessing = TRUE WHERE processing_error LIKE '%is out of range%' and type_id = '2000';
+
+-- 0000: output dates extraction issue.
+UPDATE mq.tbl_message AS m SET is_queued_for_reprocessing = TRUE WHERE m.is_queued_for_reprocessing = FALSE AND m.type_id = '0000' AND EXISTS (SELECT 1 FROM monitoring.tbl_simulation AS s WHERE s.uid = m.correlation_id_1 AND s.execution_start_date IS NOT NULL AND s.output_start_date IS NULL);
+
+-- 0000: failed due to warning delay calculation error.
+UPDATE mq.tbl_message set is_queued_for_reprocessing = TRUE WHERE processing_error LIKE '%offset-naive and offset-aware datetime%' AND type_id = '0000';
+
+-- 1000: failed due to warning delay calculation error.
+UPDATE mq.tbl_message set is_queued_for_reprocessing = TRUE WHERE processing_error LIKE '%offset-naive and offset-aware datetime%' AND type_id = '1000';
+
+-- 2000: failed due to warning delay calculation error.
+UPDATE mq.tbl_message set is_queued_for_reprocessing = TRUE WHERE processing_error LIKE '%offset-naive and offset-aware datetime%' AND type_id = '2000';
+
 
--- bash/mq/daemons_stop_phase_1.sh
+++ bash/mq/daemons_stop_phase_1.sh
@@ -8,8 +8,7 @@ main()
 {
 	log "MQ : stopping live-smtp-realtime email listener ..."
 	activate_venv
-	supervisorctl -c $HERMES_DIR_DAEMONS/mq/supervisord.conf stop live-smtp-realtime-1:01
-	supervisorctl -c $HERMES_DIR_DAEMONS/mq/supervisord.conf stop live-smtp-realtime-2:01
+	supervisorctl -c $HERMES_DIR_DAEMONS/mq/supervisord.conf stop live-smtp-realtime-1
 	log "MQ : stopped live-smtp-realtime email listener ..."
 }
 
